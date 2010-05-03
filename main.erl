12b3bfc57ae5e768f6ed214a5e3cb4c5dd27d095
Mikhail Sobolev <mss@mawhrin.net>
2010-05-03 20:37:48 +0300

    do not exit, wait for user to act

--- main.erl
+++ main.erl
@@ -5,6 +5,9 @@
 -export([start/0]).
 
 start() ->
-    io:format("great stuff will be here :)~n").
+    io:format("great stuff will be here :)~n"),
+    loop().
+
+loop() -> loop().
 
 % vim:ts=4:sw=4:et

d89863b732f272badbf8bcbef7a43a70ea22c9fa
Mikhail Sobolev <mss@mawhrin.net>
2010-05-05 19:09:57 +0300

    added a helper function for rounding up to power of two

--- utils.erl
+++ utils.erl
@@ -1,6 +1,6 @@
 -module(utils).
 
--export([select/1, index/2]).
+-export([select/1, index/2, round_to_power_of_2/1]).
 
 -import(random, [uniform/1]).
 
@@ -14,5 +14,16 @@ index(Item, List) when is_list(List) -> index(Item, List, 1).
 
 index(Item, [ Item | _Tail ], Index) -> Index;
 index(Item, [ _Head | Tail ], Index) -> index(Item, Tail, Index+1).
+
+round_to_power_of_2(Number)
+    when is_number(Number) andalso Number >= 0 ->
+
+    round_to_power_of_2_2(Number, 1).
+
+round_to_power_of_2_2(Number, Result)
+    when Number =< Result -> Result;
+
+round_to_power_of_2_2(Number, Result) ->
+    round_to_power_of_2_2(Number, Result bsl 1).
 
 % vim:ts=4:sw=4:et

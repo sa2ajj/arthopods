1ad715343e44bfd1e311bf1808ca6b672b1d418e
Mikhail Sobolev <mss@mawhrin.net>
2010-05-05 22:15:50 +0300

    started to use grass field

--- world.erl
+++ world.erl
@@ -11,11 +11,12 @@ world_loop0(Parent, Size) ->
     io:format("The world is about to revolve.~n"),
     process_flag(trap_exit, true),          % follow exits of interesting processes
     link(Parent),
+    Field = spawn(grass_field, start, [ lists:max(tuple_to_list(Size)) ]),
     Parent ! { world, self() },
     io:format("We are as big as ~p~n", [Size]),
-    world_loop(Parent, Size).
+    world_loop(Parent, Size, Field).
 
-world_loop(Parent, Size) ->
+world_loop(Parent, Size, Field) ->
     io:format("World: waiting... "),
 
     receive
@@ -27,16 +28,20 @@ world_loop(Parent, Size) ->
             io:format(" welcomed.~n");
 
         { food, Location } ->
-            io:format(" food (~p)!~n", [Location]);
+            io:format(" food (~p)!~n", [Location]),
+            Field ! { grow, Location };
 
         { size, Pid } ->
             io:format(" size requested from ~p~n", [ Pid ]),
             Pid ! { size, Size };
 
+        ack_grow ->
+            ok;
+
         Other ->
             io:format("World got: ~p~n", [ Other ])
     end,
 
-    world_loop(Parent, Size).
+    world_loop(Parent, Size, Field).
 
 % vim:ts=4:sw=4:et

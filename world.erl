b9f07dd446f1b4faf5022ae2ad12d015c4c10474
Mikhail Sobolev <mss@mawhrin.net>
2010-05-10 22:54:26 +0300

    provisioned bug storage

--- world.erl
+++ world.erl
@@ -14,9 +14,10 @@ world_loop0(Parent, Size) ->
     Field = spawn(grass_field, start, [ lists:max(tuple_to_list(Size)) ]),
     Parent ! { world, self() },
     io:format("We are as big as ~p~n", [Size]),
-    world_loop(Parent, Size, Field).
+    Bugs = dict:new(),
+    world_loop(Parent, Size, Field, Bugs).
 
-world_loop(Parent, Size, Field) ->
+world_loop(Parent, Size, Field, Bugs) ->
     io:format("World: waiting... "),
 
     receive
@@ -45,6 +46,6 @@ world_loop(Parent, Size, Field) ->
             io:format("World got: ~p~n", [ Other ])
     end,
 
-    world_loop(Parent, Size, Field).
+    world_loop(Parent, Size, Field, Bugs).
 
 % vim:ts=4:sw=4:et

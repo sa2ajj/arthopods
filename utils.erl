a590af745cbb90056a75948b913878d18acca157
Mikhail Sobolev <mss@mawhrin.net>
2010-05-17 18:15:44 +0300

    implemented a simple sleep function

--- utils.erl
+++ utils.erl
@@ -3,7 +3,7 @@
 -author("Alexey Vyskubov <alexey@mawhrin.net>").
 -author("Mikhail Sobolev <mss@mawhrin.net>").
 
--export([index/2, round_to_power_of_2/1]).
+-export([index/2, round_to_power_of_2/1, sleep/1]).
 
 index(Item, List) when is_list(List) -> index(Item, List, 1).
 
@@ -20,5 +20,11 @@ round_to_power_of_2_2(Number, Result)
 
 round_to_power_of_2_2(Number, Result) ->
     round_to_power_of_2_2(Number, Result bsl 1).
+
+sleep(T) ->
+    receive
+    after T
+        -> ok
+    end.
 
 % vim:ts=4:sw=4:et

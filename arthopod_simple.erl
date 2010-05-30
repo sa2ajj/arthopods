028c77de5551ab76d5e91579c4e50098e685248a
Mikhail Sobolev <mss@mawhrin.net>
2010-05-31 00:18:32 +0300

    completed first version of a very simple bug

--- arthopod_simple.erl
+++ arthopod_simple.erl
@@ -15,13 +15,36 @@
 
 % behaviour callback implementation
 give_birth(World, Energy) ->
-    life(#simple_bug{world=World, age=0, energy=Energy}).
+    life(#simple_bug{
+            world=World,
+            age=0,          % new born :)
+            energy=Energy,
+            direction=arthopod:random_dir(),
+            genes=make_genes(arthopod:directions(), 10)
+    }).
 
-life(Bug) ->
-    receive
-    after 0 ->
-        ok
-    end,
-    life(Bug).
+% the actual worker
+life(#simple_bug{energy=Energy}) when Energy == 0 ->
+    ok;
+
+life(#simple_bug{age=Age, energy=Energy, direction=Direction, genes=Genes} = Bug) ->
+    io:format("Bug: ~p~n", [Bug]),
+    utils:sleep(100),
+%    receive
+%    after 0 ->
+%        ok
+%    end,
+    NewDirection = arthopod:turn(Direction, select:quadratic(Genes)),
+    life(Bug#simple_bug{age=Age+1, energy=Energy-1, direction=NewDirection}).
+
+% helper functions
+make_genes(GeneTags, MaxValue) ->
+    make_genes(GeneTags, MaxValue, []).
+
+make_genes([], _, Result) ->
+    Result;
+
+make_genes([ GeneTag | Tail ], MaxValue, Result) ->
+    make_genes(Tail, MaxValue, [{GeneTag, random:uniform(MaxValue)} | Result]).
 
 % vim:ts=4:sw=4:et

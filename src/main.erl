1b8a4704d08af0d76f62a8859267eecd8096e9e9
Mikhail Sobolev <mss@mawhrin.net>
2010-06-21 15:35:58 +0300

    minor cosmetics

--- src/main.erl
+++ src/main.erl
@@ -13,7 +13,7 @@
 
 start() ->
     io:format("great stuff will be here :)~n"),
-    random:seed(now()),
+    random:seed(erlang:now()),
     Size = ?WORLD_SIZE,
     world_viewer:start_link(Size),
     arthopod_sup:start_link(),      % TODO: how to do it properly?
